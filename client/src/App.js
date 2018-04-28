5d68a9ee42fa67d2a7f4316f4cc645dcda6e3db1
Ricky <rickysychan@gmail.com>
2018-04-28 19:51:43 -0400

    added simple navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import LoginPage from './components/login.js';
+import NavigationBar from './components/navbar.js';
 
 class App extends Component {
 
@@ -42,8 +43,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
+        <NavigationBar />
         <p className="App-intro">
           {this.state.response}
         </p>
