7bc617260d464e0c6d2e0c2774f06409c63582a6
Ricky <rickysychan@gmail.com>
2018-05-01 15:54:38 -0400

    Currency information now appears on a responsive table in dashboard page

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,20 +1,114 @@
-import React from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import React from 'react'
+import socketIOClient from 'socket.io-client'
+import { Jumbotron, Button, Table } from 'react-bootstrap';
+import '../css/dashboard.css';
+
 
 class Dashboard extends React.Component {
-    render() {
+    constructor() {
+        super()
+        this.state = {
+            endpoint: "http://127.0.0.1:5000", // this is where we are connecting to with sockets
+            color: 'white',
+            data: []
+        }
+      }
+
+      componentWillMount(){
+        let grabData = () => {
+            return fetch('https://api.coinmarketcap.com/v1/ticker/')
+            .then(function(response) {
+                return response.json();
+            })
+            .then((myJson) => {
+    
+                this.setState({ data: myJson })
+                console.log(this.state.data)
+            })
+            .catch( function(err) {
+                console.log(err)
+            })
+        }
+        grabData()
+    }
+
+        // method for emitting a socket.io event
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint)
+    
+    // this emits an event to the socket (your server) with an argument of 'red'
+    // you can make the argument any color you would like, or any kind of data you want to send.
+    console.log(this.state.color)
+    socket.emit('change color', this.state.color) 
+    // socket.emit('change color', 'red', 'yellow') | you can have multiple arguments
+  }
+  
+    // adding the function
+    setColor = (color) => {
+        this.setState({ color })
+      }
+
+  // render method that renders in code if the state is updated
+  render() {
+    // Within the render method, we will be checking for any sockets.
+    // We do it in the render method because it is ran very often.
+    const socket = socketIOClient(this.state.endpoint)
+    
+    // socket.on is another method that checks for incoming events from the server
+    // This method is looking for the event 'change color'
+    // socket.on takes a callback function for the first argument
+    socket.on('change color', (color) => {
+      // setting the color of our button
+      document.body.style.backgroundColor = color
+    })
+
+    let dataObj = this.state.data
+    const data = dataObj.map((dataObj) =>
+
+        <tr> 
+            <td >{dataObj.rank}</td> 
+            <td >{dataObj.name}</td> 
+            <td >{dataObj.symbol}</td>
+            <td >{dataObj.price_btc}</td>  
+            <td >{dataObj.available_supply}</td>
+            <td >{dataObj.total_supply}</td>  
+            <td >{dataObj.percent_change_24h}</td> 
+            <td >{dataObj.price_usd}</td>
+            <form>
+                <Button type="submit" bsStyle="primary">Subscribe</Button> 
+            </form>
+        </tr>   
+    )
+
         return (
             <div>
                 <Jumbotron>
                     <h1 className="display-3">Current Crypto Currency Values</h1>
-                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
                     <hr className="my-2" />
                     <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
                     <p className="lead">
-                    <Button bsStyle="primary">Primary</Button>
+                    <Button onClick={() => this.send()} bsStyle="primary">Change color</Button>
+                    <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+                    <button id="red" onClick={() => this.setColor('red')}>Red</button>
                     </p>
                 </Jumbotron>
-
+                    <div className='table-div'>
+                        <Table responsive>
+                        <tbody>
+                            <tr>
+                                <th> Rank </th>
+                                <th>name</th>
+                                <th> Symbol </th>
+                                <th> Price </th>
+                                <th> Available Supply </th>
+                                <th> Total Supply </th>
+                                <th> last 24 hours </th>
+                                <th> Price </th>
+                            </tr>
+                                {data}
+                                </tbody>
+                        </Table>
+                    </div>
             </div>
         )
     }
--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
-const io = require('socket.io')();
+const socketIO = require('socket.io')
+const http = require('http')
 
 const app = express()
 const port = process.env.PORT || 5000
-io.listen(port)
-console.log('socket listening on port ', port)
+
 // Log requests to the console.
 app.use(logger('dev'))
 
 // Parse incoming requests data
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// our server instance
+const server = http.createServer(app)
+
+// This creates our socket using the instance of the server
+const io = socketIO(server)
+
+// This is what the socket.io syntax is like, we will work this later
+io.on('connection', socket => {
+  console.log('New client connected')
+
+  // just like on the client side, we have a socket.on method that takes a callback function
+  socket.on('change color', (color) => {
+    // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
+    // we make use of the socket.emit method again with the argument given to use from the callback function above
+    console.log('Color Changed to: ', color)
+    io.sockets.emit('change color', color)
+  })
+
+  // disconnect is fired when a client leaves the server
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
 
 // Require our routes into the application.
 require('./server/routes')(app)
@@ -21,6 +45,6 @@ app.get('*', (req, res) => {
   res.send({ express: 'Hello Bitconnect User!' })
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+server.listen(port, () => console.log(`Listening on port ${port}`))
 
 module.exports = app
